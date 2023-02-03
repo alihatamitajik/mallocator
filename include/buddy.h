fd5009fb7d10e8eb0085af73eb0e2ff949eb4286
Ali Hatami Tajik <a.hatam008@gmail.com>
2023-02-03 18:21:16 +0330

    malloc added
    not tested yet

--- include/buddy.h
+++ include/buddy.h
@@ -14,10 +14,21 @@ extern "C" {
 #define BLOCK_SIZE 48
 
 #include <stdlib.h>
+#include <stdint.h>
 
 /**
  * @brief allocates size bytes in the memory
  * 
+ * Allocation Process:
+ *  - find the best fit (equal or smallest possible) in the free allocated list
+ *  - if found an equal allocate it
+ *  - if found a bigger free block split it until fit.
+ *  - if 404 not found, then allocate twice as allocated size now and do the
+ *    splitting.
+ *  - if can't allocate new storage, return NULL.
+ * 
+ *  NOTE: it will return NULL on impossible requests (irrational or not enough
+ *        storage or not in bound)
  * 
  * @param size size to be allocated
  * @return void* 
@@ -50,16 +61,63 @@ void bud_free(void* ptr);
  */
 void bud_show_stats();
 
-typedef struct bud_block *bud_block_ptr;
+/**
+ * @brief sets minimum size that can be allocated
+ * 
+ * NOTES: 
+ *      - if min provided is bigger than the max, it will be ignored.
+ *      - if min provided in less than 0, 0 will be the min.
+ * 
+ * @param min min value
+ * @return int setted min value
+ */
+int bud_set_minimum(int min);
 
-/* block struct */
+/**
+ * @brief sets maximum size that can be allocated
+ * 
+ * NOTES: 
+ *      - if max provided is smaller than the min, it will be ignored.
+ *      - if max provided is -1, no maximum checking is done.
+ *      - max should be at least 1 or -1 for no limit
+ * 
+ * @param max max value
+ * @return int setted max value
+ */
+int bud_set_maximum(int max);
+
+typedef struct bud_block *bud_meta;
+
+/**
+ * @brief this struct will be used as metadata header
+ * 
+ * size is the order of the block allocated (i.e. 64, 128, ...) and not
+ * the size user requested.
+ * 
+ * next and prev are used for linked list structure of the memory.
+ * 
+ * is_free is freeness of the block which will be used as a marker so we can 
+ * coalesce free memories if possible.
+ * 
+ * depth is how many times split was done on the memory.
+ * 
+ * rightness is a number which ith bit of it will show weather the parent was
+ * right node or left node of the tree. It will be used to compare left and
+ * right nodes to coalesce two blocks with same parent.
+ * 
+ * ptr is the pointer returned to the user and is kept for simplicity of search
+ * process.
+ * 
+ * data is dummy pointer (no bytes is occupied to the first data block of the
+ * memory).
+ */
 struct bud_block {
     size_t size;
     struct bud_block *next;
     struct bud_block *prev;
     int is_free;
     int depth;
-    int is_left;
+    int rightness;
     void *ptr;
     /* A pointer to the allocated block */
     char data [0];
--- src/buddy.c
+++ src/buddy.c
@@ -1,17 +1,63 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+#include "buddy.h"
+
+#define MIN(a,b)             \
+({                           \
+    __typeof__ (a) _a = (a); \
+    __typeof__ (b) _b = (b); \
+    _a < _b ? _a : _b;       \
+})
+
+#define MAX(a,b)             \
+({                           \
+    __typeof__ (a) _a = (a); \
+    __typeof__ (b) _b = (b); \
+    _a > _b ? _a : _b;       \
+})
+
+bud_meta head = NULL;
+
+/** Initial Min limit (no limit) */
+size_t min_limit = 0;
+
+/** initial Max limit (no limit) */
+size_t max_limit = -1;
+
+size_t sum_allocated = 0;
+
+
+/**
+ * @brief return the smallest power of two value greater than x ([1], p 48)
+ * 
+ * Other methods was possible for this work like using A32 builtin bit scan 
+ * revers (bsr) instruction or using mathematical approach with logarithms and
+ * ceiling.
+ * 
+ * [1] Baxter, Michael. "hacker's delight." Linux Journal 2003.108 (2003).
+ *
+ */
+static __attribute__((noinline)) unsigned next_pow2(unsigned x)
+{
+	x -= 1;
+	x |= (x >> 1);
+	x |= (x >> 2);
+	x |= (x >> 4);
+	x |= (x >> 8);
+	x |= (x >> 16);
+	
+	return x + 1;
+}
+
+
 /**
  * @brief Splits the block into half
- * 
- * hard-minimum size is 64, because we should be able to keep the header meta.
- * So, hard-minimum for requested size should be 16 bytes
- * 
  * 
  * 
  * @param b the block that will be splitted - should be a valid block
  */
-int split (bud_block_ptr b)
+void split (bud_meta b)
 {
 
 }
@@ -41,27 +87,225 @@ int coalesce()
  * @param p start of allocated memory
  * @return s_block_ptr 
  */
-s_block_ptr get_block (void *p)
+bud_meta get_block (void *p)
 {
 
 }
 
 /**
- * @brief allocate memory to the end of the heap
+ * @brief double the size of the heap allocated by now
  * 
- * This function will be used when there is not enough space in the allocated
- * memory. First it will check if there is is_free space at the end. if there were
- * one, it will allocate remaining amount of memory and add it to that block. 
- * If there wasn't a is_free block at the last a new block will be constructed. at
- * any time that allocation can't be done it will return NULL.
+ * This functions double the memory we had allocated previously (sum_allocated)
+ * and update the depth of the depth of previous data.
+ * 
+ * NOTE: each time we extend the heap, depth of all blocks should be increased
+ * by one.
+ * 
+ * NOTE: since we are adding the newly block to the right, we don't need to
+ * update the rightness of the metadata.
  * 
  * @param last the last element of the block lists.
  * @param s size to be allocated
  * @return NULL on failure and a pointer to the newly allocated(expanded) block
  */
-s_block_ptr extend_heap (s_block_ptr last , size_t s)
+bud_meta extend_heap ()
+{
+    void* mem;
+    mem = sbrk(sum_allocated);
+    if (mem == (void *) -1)
+    {
+        return NULL;
+    }
+
+    bud_meta header = (bud_meta) mem;
+
+    // increment the depth of previous
+    bud_meta prev, block = head;
+    while (block)
+    {
+        block->depth += 1;
+        prev = block;
+        block = block->next;
+    }
+
+    // define the new block header
+    header->size = sum_allocated;
+    header->is_free = 1;
+    header->rightness = 1;
+    header->depth = 1;
+    header->ptr = &header->data;
+
+    // adjust the links
+    header->next = NULL;
+    header->prev = prev;
+    prev->next = header;
+
+    return header;
+}
+
+/**
+ * @brief Initializes the heap by size given to it
+ * 
+ * @param s size of allocation
+ * @return bud_meta 
+ */
+bud_meta init_heap(size_t size)
+{
+    void* mem;
+    mem = sbrk(size);
+    
+    if (mem == (void*) -1)
+    {
+        return NULL; // couldn't allocate
+    } 
+    
+    bud_meta header = (bud_meta) mem;
+    header->size = size;
+    header->is_free = 1;
+    header->next = NULL;
+    header->prev = NULL;
+    header->depth = 0;
+    header->rightness = 0;
+    header->ptr = &header->data;
+    head = header;
+
+    sum_allocated = size;
+
+    return header;
+}
+
+
+/**
+ * @brief Get the smallest fit of the data or find smallest to split
+ * 
+ * @param size 
+ * @return bud_meta 
+ */
+bud_meta get_best_fit(size_t size)
+{
+    bud_meta block = head;
+    bud_meta best = NULL;
+    while (block)
+    {
+        if (block->is_free)
+        {
+            if (block->size == size)
+            {
+                return block;
+            }
+            else if (block->size > size && (best == NULL || best->size > block->size))
+            {
+                best = block;
+            }
+        }
+        
+        block = block->next;
+    }
+
+    return best;
+}
+
+bud_meta shrink_to_size(bud_meta bm, size_t size)
+{
+    if (bm->size < size) {
+        return NULL;
+    } else {
+        while (bm->size > size)
+        {
+            split(bm);
+        }
+        return bm;
+    }
+}
+
+/**
+ * @brief Get the block pointer
+ * 
+ * for more information see the bud_malloc documentation
+ * 
+ * @param size size to get
+ * @return bud_meta NULL if couldn't else a pointer to header of *free* data
+ */
+bud_meta alloc_block(size_t size)
+{
+    
+    if (head == NULL)
+    { // if no allocation before this extend the heap
+        return init_heap(size);
+    } 
+    else 
+    { /* find the best fit block and if not found extend the heap
+         extension is done until it */
+        bud_meta best_fit = get_best_fit(size);
+        if (best_fit != NULL)
+        {
+            return shrink_to_size(best_fit, size);
+        } else { // no fitted block found, we should extend the heap
+            bud_meta last_extended;
+            do
+            {
+                last_extended = extend_heap();
+                if (last_extended == NULL)
+                    return NULL;
+            } while (sum_allocated/2 < size);
+            // shrink the new allocated heap to the size we wanted
+            return shrink_to_size(last_extended, size);
+        }
+    }
+}
+
+void* bud_malloc(size_t size)
+{
+    // if it violates the boundaries
+    if (size < min_limit || (max_limit != -1 && size > max_limit))
+    {
+        return NULL;
+    }
+    bud_meta bbp = alloc_block(next_pow2(BLOCK_SIZE + size));
+    if (bbp == NULL) 
+    {
+        return NULL;
+    } else {
+        bbp->is_free = 0;
+        return bbp->ptr;
+    }
+}
+
+
+void* bud_realloc(void* ptr, size_t size)
 {
 
 }
 
 
+void bud_free(void* ptr)
+{
+
+}
+
+void bud_show_stats()
+{
+
+}
+
+
+int bud_set_minimum(int min)
+{
+    if (min <= max_limit)
+    {
+        min_limit = MAX(0, min);
+    }
+    return min_limit;
+}
+
+
+int bud_set_maximum(int max)
+{
+    if (max == -1)
+    {
+        max_limit = -1;
+    } else if (max > min_limit)
+    {
+        max_limit = MIN(1, max);
+    }
+}
